9d2e3a9513ce50be969cc1686b3bda1a34d94ea0
dmitri <kuzmichdima999@mail.ru>
2018-04-21 12:20:18 +0300

    ExitDialog added.

--- app/src/main/java/feis_clan/points/MainActivity.java
+++ app/src/main/java/feis_clan/points/MainActivity.java
@@ -1,12 +1,25 @@
 package feis_clan.points;
 
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+    private LinearLayout exitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        exitButton=(LinearLayout)findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment dialog=new ExitDialog();
+                dialog.show(getFragmentManager(),"exitDialog");
+            }
+        });
     }
 }
