62616e2e923d76cfcaecc7234456ce76676703bc
dmitri <kuzmichdima999@mail.ru>
2018-04-21 16:16:41 +0300

    Added GameActivity w/ field 10x10.

--- app/src/main/java/feis_clan/points/GameActivity.java
+++ app/src/main/java/feis_clan/points/GameActivity.java
@@ -2,8 +2,17 @@ package feis_clan.points;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 
 public class GameActivity extends Activity{
+    GridLayout grid;
     private int redScore=0;
     private int blueScore=0;
     //private Time timeSpent;
@@ -11,5 +20,11 @@ public class GameActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.small_field);
+
+        grid=(GridLayout)findViewById(R.id.grid);
+    }
+
+    private void markupField(){
+        
     }
 }
