1d7b6d99ce1eba81aed09f4b25277b6a8f9708f5
dmitri <kuzmichdima999@mail.ru>
2018-04-24 21:08:16 +0300

    Detection of looped fields [3]. Repainting captured points [1].

--- app/src/main/java/feis_clan/points/GameActivity.java
+++ app/src/main/java/feis_clan/points/GameActivity.java
@@ -71,7 +71,9 @@ public class GameActivity extends Activity {
             cells_status[cell_id[0]][cell_id[1]] = (byte)(turn ? 1 : 2);
             ImageView image=(ImageView)findViewById(view.getId());
             image.setImageResource(turn ? R.drawable.red_cell_10px : R.drawable.blue_cell_10px);
+            maskDangerPlaces();
             checkClosedField(cell_id[0],cell_id[1]);
+            unmask();
             toggleTurn();
         }
         else {
@@ -94,8 +96,9 @@ public class GameActivity extends Activity {
         path.clear();
         sI = startI;
         sJ = startJ;
-        if(isPath(startI,startJ,cells_status[startI][startJ])){
-            StringBuilder resLoop = new StringBuilder();
+        byte color = cells_status[startI][startJ];
+        if(isPath(startI,startJ,color)){
+            /*StringBuilder resLoop = new StringBuilder();
             while (!path.empty()){
                 int last[] = path.pop();
                 resLoop.append(Integer.toString(last[0]));
@@ -103,16 +106,111 @@ public class GameActivity extends Activity {
                 resLoop.append(Integer.toString(last[1]));
                 resLoop.append(" ");
             }
-            Log.i("Result loop",resLoop.toString());
+            Log.i("Result loop",resLoop.toString());*/
+            capture((byte)(color == 1 ? 2 : 1));
         }
     }
 
     /**
-     * Use Stack to watch points I passed.
+     * Need to rework. Out of bounds in string:138
+     */
+    private void capture(byte colorToCapture){
+        //2:1 3:2 2:3 1:2 2:1
+        path.pop();
+        //3:2 2:3 1:2 2:1 <=
+        ArrayList<int[]> list = new ArrayList<>();
+        while(!path.empty()){
+            int a[] = path.pop();
+            Log.i("w", Integer.toString(a[0])+":"+Integer.toString(a[1])+" ");
+            list.add(a);
+        }
+        //3:2 2:3 1:2 2:1 in list
+        for(int i=0;i<sideSize;i++){
+            ArrayList<int[]> l = new ArrayList<>();
+            for (int[] a : list) {
+                if (a[0] == i) {
+                    l.add(a);
+                }
+            }
+            Log.i("q",l.toString());
+            if(l.size() == 1)
+                continue;
+            if(l.get(0)[1] > l.get(1)[1]){
+                l.add(l.get(0));
+                l.remove(0);
+            }
+            for(int j=l.get(0)[1];j<l.get(1)[1];j++){
+                if(cells_status[i][j] == colorToCapture)
+                    cells_status[i][j] = (byte)(colorToCapture == 1 ? 2 : 1);
+            }
+        }
+    }
+
+    private void maskDangerPlaces(){
+        //**
+        //*
+        for(int i=0;i<sideSize-1;i++){
+            for(int j=0;j<sideSize-1;j++){
+                if(cells_status[i][j] == cells_status[i][j+1] && cells_status[i][j] == cells_status[i+1][j]
+                        && cells_status[i][j] != 0){
+                    cells_status[i][j] = (byte)(cells_status[i][j] == 1 ? -1 : -2);
+                }
+            }
+        }
+        //**
+        // *
+        for(int i=0;i<sideSize-1;i++){
+            for(int j=0;j<sideSize-1;j++){
+                if(cells_status[i][j] == cells_status[i][j+1] && cells_status[i][j] == cells_status[i+1][j+1]
+                        && cells_status[i][j] != 0){
+                    cells_status[i][j+1] = (byte)(cells_status[i][j] == 1 ? -1 : -2);
+                }
+            }
+        }
+        // *
+        //**
+        for(int i=0;i<sideSize-1;i++){
+            for(int j=1;j<sideSize;j++){
+                if(cells_status[i][j] == cells_status[i+1][j] && cells_status[i][j] == cells_status[i+1][j-1]
+                        && cells_status[i][j] != 0){
+                    cells_status[i][j] = (byte)(cells_status[i+1][j] == 1 ? -1 : -2);
+                }
+            }
+        }
+        //*
+        //**
+        for(int i=0;i<sideSize-1;i++){
+            for(int j=0;j<sideSize-1;j++){
+                if(cells_status[i][j] == cells_status[i+1][j] && cells_status[i][j] == cells_status[i+1][j+1]
+                        && cells_status[i][j] != 0){
+                    cells_status[i+1][j] = (byte)(cells_status[i+1][j] == 1 ? -1 : -2);
+                }
+            }
+        }
+    }
+
+    private void unmask(){
+        for(int i=0;i<sideSize;i++){
+            for(int j=0;j<sideSize;j++){
+                if(cells_status[i][j] == -1){
+                    cells_status[i][j] = 1;
+                }
+                if(cells_status[i][j] == -2){
+                    cells_status[i][j] = 2;
+                }
+            }
+        }
+    }
+
+    /**
+     *  | |*| |
+     *  |*|#|*| <= crash
+     *  | |*|*|
      *
-     *  | |*| |
-     *  |*|#|*|
-     *  | |*| |
+     *  |*| | | | |
+     *  | |*|*|*| |
+     *  | | |*| | |
+     *  | | | | | |
      */
 
     private boolean isPath(int curI, int curJ, byte color){
@@ -133,41 +231,21 @@ public class GameActivity extends Activity {
                     path.pop();
             }
         }
-        if(isMovable(curI,curJ,2) == color){
-            if(visited[curI-1][curJ+1] == 0 ||
-                    (visited[curI-1][curJ+1] == 1 && !(path.lastElement()[0] == curI-1 && path.lastElement()[1] == curJ+1))){
-                path.push(new int[]{curI,curJ});
-                if(isPath(curI-1,curJ+1,color)){
-                    return true;
-                }else
-                    path.pop();
-            }
-        }
-        if(isMovable(curI,curJ,5) == color){
-            if(visited[curI+1][curJ-1] == 0 ||
-                    (visited[curI+1][curJ-1] == 1 && !(path.lastElement()[0] == curI+1 && path.lastElement()[1] == curJ-1))){
-                path.push(new int[]{curI,curJ});
-                if(isPath(curI+1,curJ-1,color)){
-                    return true;
-                }else
-                    path.pop();
-            }
-        }
-        if(isMovable(curI,curJ,7) == color){
-            if(visited[curI+1][curJ+1] == 0 ||
-                    (visited[curI+1][curJ+1] == 1 && !(path.lastElement()[0] == curI+1 && path.lastElement()[1] == curJ+1))){
-                path.push(new int[]{curI,curJ});
-                if(isPath(curI+1,curJ+1,color)){
-                    return true;
-                }else
-                    path.pop();
-            }
-        }
         if(isMovable(curI,curJ,1) == color){
             if(visited[curI-1][curJ] == 0 ||
                     (visited[curI-1][curJ] == 1 && !(path.lastElement()[0] == curI-1 && path.lastElement()[1] == curJ))){
                 path.push(new int[]{curI,curJ});
                 if(isPath(curI-1,curJ,color)){
+                    return true;
+                }else
+                    path.pop();
+            }
+        }
+        if(isMovable(curI,curJ,2) == color){
+            if(visited[curI-1][curJ+1] == 0 ||
+                    (visited[curI-1][curJ+1] == 1 && !(path.lastElement()[0] == curI-1 && path.lastElement()[1] == curJ+1))){
+                path.push(new int[]{curI,curJ});
+                if(isPath(curI-1,curJ+1,color)){
                     return true;
                 }else
                     path.pop();
@@ -193,11 +271,31 @@ public class GameActivity extends Activity {
                     path.pop();
             }
         }
+        if(isMovable(curI,curJ,5) == color){
+            if(visited[curI+1][curJ-1] == 0 ||
+                    (visited[curI+1][curJ-1] == 1 && !(path.lastElement()[0] == curI+1 && path.lastElement()[1] == curJ-1))){
+                path.push(new int[]{curI,curJ});
+                if(isPath(curI+1,curJ-1,color)){
+                    return true;
+                }else
+                    path.pop();
+            }
+        }
         if(isMovable(curI,curJ,6) == color){
             if(visited[curI+1][curJ] == 0 ||
                     (visited[curI+1][curJ] == 1 && !(path.lastElement()[0] == curI+1 && path.lastElement()[1] == curJ))){
                 path.push(new int[]{curI,curJ});
                 if(isPath(curI+1,curJ,color)){
+                    return true;
+                }else
+                    path.pop();
+            }
+        }
+        if(isMovable(curI,curJ,7) == color){
+            if(visited[curI+1][curJ+1] == 0 ||
+                    (visited[curI+1][curJ+1] == 1 && !(path.lastElement()[0] == curI+1 && path.lastElement()[1] == curJ+1))){
+                path.push(new int[]{curI,curJ});
+                if(isPath(curI+1,curJ+1,color)){
                     return true;
                 }else
                     path.pop();
@@ -223,42 +321,42 @@ public class GameActivity extends Activity {
      */
     private byte isMovable(int i, int j, int direction){
         if(direction == 0){
-            if(i != 0 && j!= 0)
+            if(i > 0 && j > 0)
                 return cells_status[i-1][j-1];
             else return 0;
         }
         else if(direction == 1){
-            if(i != 0)
+            if(i > 0)
                 return cells_status[i-1][j];
             else return 0;
         }
         else if(direction == 2){
-            if(i != 0 && j != sideSize-1)
+            if(i > 0 && j < sideSize-1)
                 return cells_status[i-1][j+1];
             else return 0;
         }
         else if(direction == 3){
-            if(j != 0)
+            if(j > 0)
                 return cells_status[i][j-1];
             else return 0;
         }
         else if(direction == 4){
-            if(j != sideSize-1)
+            if(j < sideSize-1)
                 return cells_status[i][j+1];
             else return 0;
         }
         else if(direction == 5){
-            if(i != sideSize-1 && j != 0)
+            if(i < sideSize-1 && j > 0)
                 return cells_status[i+1][j-1];
             else return 0;
         }
         else if(direction == 6){
-            if(i != sideSize-1)
+            if(i < sideSize-1)
                 return cells_status[i+1][j];
             else return 0;
         }
         else {
-            if(i != sideSize-1 && j != sideSize-1)
+            if(i < sideSize-1 && j < sideSize-1)
                 return cells_status[i+1][j+1];
             else return 0;
         }
