031e527162169dd8715deb8f665133541eb036a1
dmitri <kuzmichdima999@mail.ru>
2018-04-29 15:09:02 +0300

    Capturing surrounded points - Done.

--- app/src/main/java/feis_clan/points/GameActivity.java
+++ app/src/main/java/feis_clan/points/GameActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Stack;
 
 public class GameActivity extends Activity {
@@ -38,6 +39,10 @@ public class GameActivity extends Activity {
         cells_status = getEmptyArray();
     }
 
+    /**
+     * Fills the field (GridLayout) with ImageView's
+     * and sets ID and OnClickListener for them.
+     */
     private void markupField() {
         grid.setColumnCount(sideSize);
         for (int i = 0; i < square; i++) {
@@ -49,6 +54,9 @@ public class GameActivity extends Activity {
         }
     }
 
+    /**
+     * OnClickListener for points on the field.
+     */
     private View.OnClickListener cellClick=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -56,31 +64,45 @@ public class GameActivity extends Activity {
         }
     };
 
-    private void makeShortToast(Context context, String message){
-        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+    /**
+     * Generate short Toast when player tries to place his point on already occupied point.
+     */
+    private void filledPointToast(Context context){
+        Toast.makeText(context,"There is a point already!",Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * Toggles the turn after one of players placed his point.
+     */
     private void toggleTurn(){
         turn = !turn;
     }
 
+    /**
+     * Sets just placed point in matrix of points.
+     * Runs methods to check captured zones.
+     * @param view - view of just placed point.
+     */
     private void setPoint(View view){
         int cell_id[] = getIDbyGridID(view.getId());
         if(cells_status[cell_id[0]][cell_id[1]] == 0){
             cells_status[cell_id[0]][cell_id[1]] = (byte)(turn ? 1 : 2);
             ImageView image=(ImageView)findViewById(view.getId());
             image.setImageResource(turn ? R.drawable.red_cell_10px : R.drawable.blue_cell_10px);
-            //maskDangerousPlaces();
             checkClosedField(cell_id[0],cell_id[1]);
-            //unmask();
             recountScore();
             toggleTurn();
         }
         else {
-            makeShortToast(getApplicationContext(), "There is a point already!");
+            filledPointToast(getApplicationContext());
         }
     }
 
+    /**
+     * Gets (x,y) coordinates of point by ID of point in GridLayout.
+     * @param id - ID of point in GridLayout.
+     * @return array of coordinates (e.g. [3,2])
+     */
     private int[] getIDbyGridID(int id){
         int cellID[] = {0, 0};
         while (id > sideSize){
@@ -91,6 +113,11 @@ public class GameActivity extends Activity {
         return cellID;
     }
 
+    /**
+     * Checks if just placed point finishes any loop of points of the same color.
+     * @param startI - coordinate y of just placed point.
+     * @param startJ - coordinate x of just placed point.
+     */
     private void checkClosedField(int startI, int startJ){
         visited = getEmptyArray();
         path.clear();
@@ -98,15 +125,6 @@ public class GameActivity extends Activity {
         sJ = startJ;
         byte color = cells_status[startI][startJ];
         if(isPath(startI,startJ,color)){
-            /*StringBuilder resLoop = new StringBuilder();
-            while (!path.empty()){
-                int last[] = path.pop();
-                resLoop.append(Integer.toString(last[0]));
-                resLoop.append(":");
-                resLoop.append(Integer.toString(last[1]));
-                resLoop.append(" ");
-            }
-            Log.i("Result loop",resLoop.toString());*/
             if(turn){
                 redScore += capture((byte)2);
             }else
@@ -114,6 +132,10 @@ public class GameActivity extends Activity {
         }
     }
 
+    /**
+     * Recounts the score according the amount of points on the field.
+     * Score directly depends on amount of points on the field.
+     */
     private void recountScore(){
         redScore = blueScore = 0;
         for(int i=0;i<sideSize;i++){
@@ -124,29 +146,20 @@ public class GameActivity extends Activity {
                     blueScore++;
             }
         }
-        ((TextView)findViewById(R.id.redScoreText)).setText(Integer.toString(redScore));
-        ((TextView)findViewById(R.id.blueScoreText)).setText(Integer.toString(blueScore));
+        ((TextView)findViewById(R.id.redScoreText)).setText(String.format(Locale.getDefault(),"%d", redScore));
+        ((TextView)findViewById(R.id.blueScoreText)).setText(String.format(Locale.getDefault(),"%d", blueScore));
     }
 
     /**
-     * | | | |*| |
-     * | | |*| |*|
-     * | |*|&| |*|
-     * | | |*|*| |
-     * | | | | | |
-     */
-
-    /**
-     * TODO
-     * чекать каждую точку на предмет ударения с соседними точками (в 4 направления) другого цвета
+     * Captures occupied points.
+     * @param colorToCapture - color of points that can be captured.
+     * @return - the amount of captured points.
      */
     private int capture(byte colorToCapture){
-        //2:1 3:2 2:3 1:2 2:1
         int count = 0;
         path.pop();
         ArrayList<int[]> list = new ArrayList<>(path);
-        Log.i("qwe", Integer.toString(list.size()));
-        //[3,2] [2,3] [1,2] [2,1]
+        wallToString(list);
 
         for(int i=0;i<sideSize;i++){
             for(int j=0;j<sideSize;j++){
@@ -166,6 +179,13 @@ public class GameActivity extends Activity {
         return count;
     }
 
+    /**
+     * Checks if the point with given coordinates is surrounded by points of opposite color.
+     * @param wall - ArrayList of loop-forming points.
+     * @param curI - y coordinate of current point.
+     * @param curJ - x coordinate of current point.
+     * @return true if point is surrounded, false - other way.
+     */
     private boolean isSurrounded(ArrayList<int[]> wall, int curI, int curJ){
         if(isUpLocked(wall, curI, curJ, cells_status[curI][curJ])){
             Log.i("q", "Point "+Integer.toString(curI)+":"+Integer.toString(curJ)+" is upLocked");
@@ -180,98 +200,137 @@ public class GameActivity extends Activity {
         return false;
     }
 
+    /**
+     * Builds a string of points' coordinates and prints the string to Log.
+     * @param wall - ArrayList of loop-forming points.
+     */
+    private void wallToString(ArrayList<int[]> wall){
+        StringBuilder builder = new StringBuilder();
+        for (int[] aWall : wall) {
+            builder.append(aWall[0]);
+            builder.append(":");
+            builder.append(aWall[1]);
+            builder.append(" ");
+        }
+        Log.i("wall", "The wall is "+builder.toString());
+    }
+
+    /**
+     * 4 methods below check if point is locked from top/left/right/bottom by points from loop-forming array.
+     * @param wall - loop-forming array of points.
+     * @param curI - y coordinate of current point.
+     * @param curJ - x coordinate of current point.
+     * @param checkingPointColor - color of current point.
+     * @return true if point is locked from top/left/right/bottom, false - other way.
+     */
     private boolean isUpLocked(ArrayList<int[]> wall, int curI, int curJ, byte checkingPointColor) {
         byte isMove = isMovable(curI, curJ, 1);
-        if(isMove == getOpposite(checkingPointColor) && wall.contains(new int[]{curI-1,curJ}))
+        if (isMove == getOpposite(checkingPointColor) && isFromWall(wall, curI - 1, curJ)) {
             return true;
-        if(isMove != -10){
-            return isUpLocked(wall, curI-1, curJ, checkingPointColor);
-        }else return false;
+        }
+        return isMove != -10 && isUpLocked(wall, curI - 1, curJ, checkingPointColor);
     }
 
-    private boolean isLeftLocked(ArrayList<int[]> wall, int curI, int curJ, byte checkingPointColor){
+    private boolean isLeftLocked(ArrayList<int[]> wall, int curI, int curJ, byte checkingPointColor) {
         byte isMove = isMovable(curI, curJ, 3);
-        if(isMove == getOpposite(checkingPointColor) && wall.contains(new int[]{curI,curJ-1}))
+        if (isMove == getOpposite(checkingPointColor) && isFromWall(wall, curI, curJ - 1))
             return true;
-        if(isMove != -10){
-            return isLeftLocked(wall, curI, curJ-1, checkingPointColor);
-        }else return false;
+        return isMove != -10 && isLeftLocked(wall, curI, curJ - 1, checkingPointColor);
     }
 
-    private boolean isRightLocked(ArrayList<int[]> wall, int curI, int curJ, byte checkingPointColor){
+    private boolean isRightLocked(ArrayList<int[]> wall, int curI, int curJ, byte checkingPointColor) {
         byte isMove = isMovable(curI, curJ, 4);
-        if(isMove == getOpposite(checkingPointColor) && wall.contains(new int[]{curI,curJ+1}))
+        if (isMove == getOpposite(checkingPointColor) && isFromWall(wall, curI, curJ + 1))
             return true;
-        if(isMove != -10){
-            return isRightLocked(wall, curI, curJ+1, checkingPointColor);
-        }else return false;
+        return isMove != -10 && isRightLocked(wall, curI, curJ + 1, checkingPointColor);
     }
 
-    private boolean isDownLocked(ArrayList<int[]> wall, int curI, int curJ, byte checkingPointColor){
+    private boolean isDownLocked(ArrayList<int[]> wall, int curI, int curJ, byte checkingPointColor) {
         byte isMove = isMovable(curI, curJ, 6);
-        if(isMove == getOpposite(checkingPointColor) && wall.contains(new int[]{curI+1,curJ}))
+        if (isMove == getOpposite(checkingPointColor) && isFromWall(wall, curI + 1, curJ))
             return true;
-        if(isMove != -10){
-            return isDownLocked(wall, curI+1, curJ, checkingPointColor);
-        }else return false;
+        return isMove != -10 && isDownLocked(wall, curI + 1, curJ, checkingPointColor);
     }
 
-    private void maskDangerousPlaces(){
-        //**
-        //*
-        for(int i=0;i<sideSize-1;i++){
-            for(int j=0;j<sideSize-1;j++){
-                if(cells_status[i][j] == cells_status[i][j+1] && cells_status[i][j] == cells_status[i+1][j]
-                        && cells_status[i][j] != 0){
-                    cells_status[i][j] = (byte)(cells_status[i][j] == 1 ? -1 : -2);
-                }
+    /**
+     * Checks if given point belongs to given array of points.
+     * @param wall - ArrayList of points.
+     * @param i - y coordinate of given point.
+     * @param j - x coordinate of given point.
+     * @return true if point belongs to wall, false - other way.
+     */
+    private boolean isFromWall(ArrayList<int[]> wall, int i, int j){
+        for(int[] point : wall){
+            if(point[0] == i && point[1] == j){
+                return true;
             }
         }
-        //**
-        // *
-        for(int i=0;i<sideSize-1;i++){
-            for(int j=0;j<sideSize-1;j++){
-                if(cells_status[i][j] == cells_status[i][j+1] && cells_status[i][j] == cells_status[i+1][j+1]
-                        && cells_status[i][j] != 0){
-                    cells_status[i][j+1] = (byte)(cells_status[i][j] == 1 ? -1 : -2);
-                }
-            }
-        }
-        // *
-        //**
-        for(int i=0;i<sideSize-1;i++){
-            for(int j=1;j<sideSize;j++){
-                if(cells_status[i][j] == cells_status[i+1][j] && cells_status[i][j] == cells_status[i+1][j-1]
-                        && cells_status[i][j] != 0){
-                    cells_status[i][j] = (byte)(cells_status[i+1][j] == 1 ? -1 : -2);
-                }
-            }
-        }
-        //*
-        //**
-        for(int i=0;i<sideSize-1;i++){
-            for(int j=0;j<sideSize-1;j++){
-                if(cells_status[i][j] == cells_status[i+1][j] && cells_status[i][j] == cells_status[i+1][j+1]
-                        && cells_status[i][j] != 0){
-                    cells_status[i+1][j] = (byte)(cells_status[i+1][j] == 1 ? -1 : -2);
-                }
-            }
-        }
+        return false;
     }
 
-    private void unmask(){
-        for(int i=0;i<sideSize;i++){
-            for(int j=0;j<sideSize;j++){
-                if(cells_status[i][j] == -1){
-                    cells_status[i][j] = 1;
-                }
-                if(cells_status[i][j] == -2){
-                    cells_status[i][j] = 2;
-                }
-            }
-        }
-    }
+//    private void maskDangerousPlaces(){
+//        //**
+//        //*
+//        for(int i=0;i<sideSize-1;i++){
+//            for(int j=0;j<sideSize-1;j++){
+//                if(cells_status[i][j] == cells_status[i][j+1] && cells_status[i][j] == cells_status[i+1][j]
+//                        && cells_status[i][j] != 0){
+//                    cells_status[i][j] = (byte)(cells_status[i][j] == 1 ? -1 : -2);
+//                }
+//            }
+//        }
+//        //**
+//        // *
+//        for(int i=0;i<sideSize-1;i++){
+//            for(int j=0;j<sideSize-1;j++){
+//                if(cells_status[i][j] == cells_status[i][j+1] && cells_status[i][j] == cells_status[i+1][j+1]
+//                        && cells_status[i][j] != 0){
+//                    cells_status[i][j+1] = (byte)(cells_status[i][j] == 1 ? -1 : -2);
+//                }
+//            }
+//        }
+//        // *
+//        //**
+//        for(int i=0;i<sideSize-1;i++){
+//            for(int j=1;j<sideSize;j++){
+//                if(cells_status[i][j] == cells_status[i+1][j] && cells_status[i][j] == cells_status[i+1][j-1]
+//                        && cells_status[i][j] != 0){
+//                    cells_status[i][j] = (byte)(cells_status[i+1][j] == 1 ? -1 : -2);
+//                }
+//            }
+//        }
+//        //*
+//        //**
+//        for(int i=0;i<sideSize-1;i++){
+//            for(int j=0;j<sideSize-1;j++){
+//                if(cells_status[i][j] == cells_status[i+1][j] && cells_status[i][j] == cells_status[i+1][j+1]
+//                        && cells_status[i][j] != 0){
+//                    cells_status[i+1][j] = (byte)(cells_status[i+1][j] == 1 ? -1 : -2);
+//                }
+//            }
+//        }
+//    }
+//
+//    private void unmask(){
+//        for(int i=0;i<sideSize;i++){
+//            for(int j=0;j<sideSize;j++){
+//                if(cells_status[i][j] == -1){
+//                    cells_status[i][j] = 1;
+//                }
+//                if(cells_status[i][j] == -2){
+//                    cells_status[i][j] = 2;
+//                }
+//            }
+//        }
+//    }
 
+    /**
+     * Finds the loop starting from given point.
+     * @param curI - y coordinate of given point.
+     * @param curJ - x coordinate of given point.
+     * @param color - color of given point.
+     * @return true if the path was found, false - other way.
+     */
     private boolean isPath(int curI, int curJ, byte color){
         if(curI == sI && curJ == sJ){
             if(path.size() != 0){
@@ -382,10 +441,15 @@ public class GameActivity extends Activity {
     }
 
     /**
-     * direction:
+     * Checks if the move can be performed to the given direction from given point.
+     * @param i - y coordinate of given point.
+     * @param j - x coordinate of given point.
+     * @param direction:
      * 0 - top-left   1 - top   2 - top-right
      * 3 - left   4 - right
      * 5 - bottom-left   6 - bottom   7 - bottom-right
+     * @return color of point placed to the given direction from given point,
+     * returns -10 if bounds of field reached.
      */
     private byte isMovable(int i, int j, int direction){
         if(direction == 0){
